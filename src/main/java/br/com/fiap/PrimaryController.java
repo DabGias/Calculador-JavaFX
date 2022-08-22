52ca9eca2c18af11e6fb8f4f136724fb45a99cc2
Gabriel Dias <101650391+DabGias@users.noreply.github.com>
2022-08-22 10:54:49 -0300

    Adição do try - catch e Exceptions

--- src/main/java/br/com/fiap/PrimaryController.java
+++ src/main/java/br/com/fiap/PrimaryController.java
@@ -18,20 +18,33 @@ public class PrimaryController {
 
     @FXML
     private void calculaVantagem() {
-        double precoGas = Double.valueOf(precoGasolina.getText());
-        double precoAlc = Double.valueOf(precoAlcool.getText());
-        double resultado = precoAlc / precoGas;
-
-        if (resultado <= 0.7) {
+        try {
+            double precoGas = Double.valueOf(precoGasolina.getText());
+            double precoAlc = Double.valueOf(precoAlcool.getText());
+            
+            if (precoAlc <= 0 || precoGas <= 0) {
+                throw new IllegalArgumentException("Número negativo");
+            }
+            
+            double resultado = precoAlc / precoGas;
+            
+            if (resultado <= 0.7) {
             lblResposta.setText("Sim");
-        } else {
-            lblResposta.setText("Não");
-        }
+            } else {
+                lblResposta.setText("Não");
+            }
 
+        } catch (NumberFormatException erro) {
+            mostraMensagem("Entrada de dados inválida. O dado deve ser um número inteiro ou decimal!");
+        } catch (IllegalArgumentException erro) {
+            mostraMensagem("Entrada de dados inválida. O número deve ser positivo!")
+        }
     }
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void mostraMensagem(String texto) {
+        Alert mensagem = new Alert(AlertType.ERROR);
+        mensagem.setContentText(texto);
+        mensagem.setTitle("Erro");
+        mensagem.show();
     }
 }
